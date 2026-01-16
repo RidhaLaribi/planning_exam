f0846beabca5b7003e5dd89dca2a76a5e3eefc9d
RidhaLaribi <ridalaribi26@gmail.com>
2026-01-16 04:35:04 +0100

    Add CORS and Hello World test route

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,6 @@ Route::get('/login', function () {
 // Temporary Seeding Route
 Route::get('/seed', function () {
     try {
-        \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true]);
         return response()->json(['message' => 'Database seeded successfully', 'output' => \Illuminate\Support\Facades\Artisan::output()]);
     } catch (\Exception $e) {
         return response()->json(['error' => $e->getMessage()], 500);
